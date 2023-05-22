1d55355b012ce3e6f16e37db3784a97e336f6d1c
adee012 <adedwiputra16@gmail.com>
2023-05-22 11:20:33 +0700

    [FEAT] Task revoU Katik

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/8DBNpxcv)
+
 # autograder-v2
-New version of autograder
+
+Hi there &#128075;
+
+This is my revoU task &#128512;
+
+- Kalkulator Matematik
+  - Hitung luas persegi
+  - Hitung keliling persegi
+
+If interested in me, visit https://www.instagram.com/ade.designn/?hl=id
---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+// function hitung luas start
+function luas() {
+  let nilaiLuas = document.getElementById("nilai-luas").value;
+
+  if (!nilaiLuas) {
+    alert("Inputkan Sisi Terlebih Dahulu");
+  } else {
+    let hasil = nilaiLuas * nilaiLuas;
+    document.getElementById("result-luas").innerHTML =
+      "<p>L = S x S</p> <p>L = " +
+      nilaiLuas +
+      " x " +
+      nilaiLuas +
+      " </p> <p>L = " +
+      hasil +
+      "</p>";
+  }
+}
+
+function resetLuas() {
+  document.getElementById("nilai-luas").value = "";
+  document.getElementById("result-luas").innerHTML = "";
+}
+// function hitung luas end
+
+// function hitung keliling start
+function keliling() {
+  let nilaiKeliling = document.getElementById("nilai-keliling").value;
+
+  if (!nilaiKeliling) {
+    alert("Inputkan Sisi Terlebih Dahulu");
+  } else {
+    let hasil = 4 * nilaiKeliling;
+    document.getElementById("result-keliling").innerHTML =
+      "<p>K = 4 x S</p> <p>K = 4 x " +
+      nilaiKeliling +
+      " </p> <p>K = " +
+      hasil +
+      "</p>";
+  }
+}
+
+function resetkeliling() {
+  document.getElementById("nilai-keliling").value = "";
+  document.getElementById("result-keliling").innerHTML = "";
+}
+// function hitung keliling end
